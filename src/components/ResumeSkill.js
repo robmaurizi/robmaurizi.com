90ee057e5ebcfaa3228609a9e4c9c419362c351f
Rob Maurizi <5453956+robmaurizi@users.noreply.github.com>
2021-02-17 09:58:23 -0500

    Single-bar skils graph with GSAP

--- src/components/ResumeSkill.js
+++ src/components/ResumeSkill.js
@@ -1,19 +1,19 @@
 import React from 'react';
+import { Tween, Reveal } from 'react-gsap';
 
 import '../styles/ResumeSkill.scss';
 
 const ResumeSkill = ( {skill, rating} ) => {
+
+    const tweenEnd = `${rating * (100/5)}%`;
+
     return (
-        <div className="resume-skill">
+        <div className="resume-skill" title={`${skill}: ${ rating } / 5`}>
             <h3 className="resume-skill-title item-head">{skill}</h3>
             <span className="resume-skill-rating" data-rating={rating}>
                 <span className="resume-skill-rating-text">{ rating } / 5</span>
                 <span className="resume-skill-rating-graph">
-                    <i></i>
-                    <i></i>
-                    <i></i>
-                    <i></i>
-                    <i></i>
+                    <Reveal><Tween from={{width: 0}} to={{ width: tweenEnd}} duration={1} delay={0.5}><i></i></Tween></Reveal>
                 </span>
             </span>
         </div>
